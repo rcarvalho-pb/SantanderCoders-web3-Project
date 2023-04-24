4b422885b432695dffff9af833d28e4884f70c7c
Ramon Carvalho <rcarvalho.pb@gmail.com>
2023-04-24 07:05:13 -0300

    FeingClient sem funcionar no reactor

--- Filme/src/main/java/com/ada/web3/projeto/Filme/model/MovieMinimal.java
+++
@@ -1,2 +0,0 @@
-package com.ada.web3.projeto.Filme.model;public class MovieMinimal {
-}
--- Filme/src/main/java/com/ada/web3/projeto/Filme/model/ResultList.java
+++ Filme/src/main/java/com/ada/web3/projeto/Filme/model/ResultList.java
@@ -1,13 +1,14 @@
 package com.ada.web3.projeto.Filme.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
+
 import java.util.List;
 
+@Data
+@Getter
 public class ResultSearch {
-
     @JsonProperty("Search")
-    private List<MovieMinimal> resultList;
-    private Integer total;
-    private Boolean response;
+    private List<Filme> resultList;
 }
-se;
